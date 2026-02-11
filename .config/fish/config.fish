e283bac6797a808a5e389cf817a9bfa28133608d
= <andrucreatinggames@gmail.com>
2026-02-11 11:47:37 +0600

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+set -U fish_greeting
+
+# Created by `pipx` on 2026-02-09 10:08:20
+set PATH $PATH /home/mrangames/.local/bin
+set -x XDG_DATA_DIRS $XDG_DATA_DIRS /var/lib/flatpak/exports/share /home/$USER/.local/share/flatpak/exports/share
+
